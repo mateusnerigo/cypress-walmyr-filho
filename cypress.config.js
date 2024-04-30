02000078feec33a6d0a0d5446427cba05b3158c7
NeriGo <mateuslimaneri@gmail.com>
2024-04-29 22:18:34 -0300

    Refactoring CRUD funcionalities

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,7 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://notes-serverless-app.com',
     defaultCommandTimeout: 10000,
+    chromeWebSecurity: false,
     // setupNodeEvents(on, config) {
     //   // implement node event listeners here
     // },
